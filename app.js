31ce81915193048bef242406bbf5af3b3c40b954
= <j0.eritzian@hotmail.fr>
2016-12-01 21:28:43 +0100

    Add language example

--- app.js
+++ app.js
@@ -23,6 +23,31 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session) {
+        // Prompt the user to select their preferred locale
+        builder.Prompts.choice(session, "What's your preferred language?", 'English|Español|Italiano');
+    },
+    function (session, results) {
+        // Update preferred locale
+        var locale;
+        switch (results.response.entity) {
+            case 'English':
+                locale = 'en';
+            case 'Español':
+                locale = 'es';
+            case 'Italiano':
+                locale = 'it';
+                break;
+        }
+        session.preferredLocale(locale, function (err) {
+            if (!err) {
+                // Locale files loaded
+                session.endDialog("Your preferred language is now %s.", results.response.entity);
+            } else {
+                // Problem loading the selected locale
+                session.error(err);
+            }
+        });
+    }
+]);
